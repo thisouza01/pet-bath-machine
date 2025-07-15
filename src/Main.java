cc4dcbf991ee75cbc6b049046d2346ff1a732ca1
Thiago <tihsouza01@gmail.com>
2025-07-15 16:31:02 -0300

    Add: construtor da classe PetMachine.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args){
+        var petMachine = new PetMachine();
 
+        System.out.println(petMachine.waterLevel());
     }
 }
--- src/PetMachine.java
+++ src/PetMachine.java
@@ -1,7 +1,49 @@
 public class PetMachine {
     private boolean isClean = true;
-    private int water = 0;
+    private boolean hasPet = false;
+    private int water = 100;
     private int shampoo = 0;
+
+    // constructor
+    public PetMachine() {
+        this.isClean = isClean;
+        this.hasPet = hasPet;
+        this.water = water;
+        this.shampoo = shampoo;
+    }
+
+    // banho do pet
+    public void petBath(){
+        if (waterLevel() <= 0) {
+            System.out.println("O nível de água está zerado!");
+        }
+    }
+
+    // abastecer a agua
+
+    // abastecer o shampoo
+
+    // verificar nivel de agua
+    public int waterLevel(){
+        return getWater();
+    }
+    // verificar nivel de shampoo
+
+    // verificar se tem pet no banho
+
+    // colocar pet na maquina
+
+    // retirar pet da maquina
+
+    // limpar maquina
+
+
+    // getters
+
+    public int getWater() {
+        return water;
+    }
+    public int getShampoo() {
+        return shampoo;
+    }
 }
-
-
