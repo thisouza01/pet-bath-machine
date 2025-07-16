27cb70f44859b362b19a9e043a2c66b9ecbdd0bc
Thiago <tihsouza01@gmail.com>
2025-07-16 08:09:30 -0300

    Add: classe Pet.

--- src/Pet.java
+++ src/Pet.java
@@ -1,2 +1,19 @@
 public class Pet {
+    private final String name;
+    private boolean clean;
+
+    // constructor
+    public Pet(String name) {
+        this.name = name;
+        this.clean = false;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public boolean isClean() {
+        return clean;
+    }
 }
