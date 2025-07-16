afc5c3e3b963b115dc488cc64fad02e4ea9b5a2a
Thiago <tihsouza01@gmail.com>
2025-07-16 08:25:00 -0300

    Fix: Alterado nome do método.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args){
         var petMachine = new PetMachine();
 
-        petMachine.takeShower();
+        petMachine.takeAShower();
     }
 }
--- src/PetMachine.java
+++ src/PetMachine.java
@@ -1,50 +1,67 @@
 public class PetMachine {
-    private boolean isClean = true;
-    private boolean hasPet = false;
-    private int water = 0;
-    private int shampoo = 0;
-
+    private boolean clean;
+    private int water;
+    private int shampoo;
     private Pet pet;
 
     // banho do pet
-    public void takeShower(){
-        if (waterLevel() <= 0) {
-            System.out.println("O nível de água está zerado!");
+    public void takeAShower(){
+        if (this.pet == null){
+            System.out.println("Coloque o pet na máquina, para iniciar o banho");
+            return;
         }
 
+        this.water -= 10;
+        this.shampoo -= 2;
         pet.setClean(true);
-    }
-
-    public boolean setClean(boolean isClean){
-        return isClean;
+        System.out.println("O pet " + pet.getName() + " está limpo!");
     }
 
     // abastecer a agua
-    public void putWater(){
-        this.water += 2;
+    public void addWater(){
+        if (water == 30){
+            System.out.println("A capacidade de água está no limite!");
+            return;
+        }
+        water += 2;
     }
 
-    // abastecer o shampoo
-    public void putShampoo(){
-        this.shampoo += 2;
+    public void addShampoo(){
+        if (shampoo == 10){
+            System.out.println("A capacidade do shampoo está no limite!");
+            return;
+        }
+        shampoo += 2;
     }
 
     // verificar nivel de agua
     public int waterLevel(){
-        return getWater();
+        return water;
     }
 
     // verificar nivel de shampoo
     public int shampooLevel(){
-        return getShampoo();
+        return shampoo;
     }
+
     // verificar se tem pet no banho
+    public boolean hasPet(){
+        return pet != null;
+    }
 
     // colocar pet na maquina
     public void putPetInTheMachine(){
-        if (this.pet != null){
-            System.out.println("Já tem um pet na máquina!");
+        if (!this.clean) {
+            System.out.println("A máquina está suja. Para colocar um pet, terá que limpa-la!");
+            return;
         }
+
+        if (hasPet()){
+            System.out.println("Já tem um pet na máquina!");
+            return;
+        }
+
+
     }
 
     // retirar pet da maquina
@@ -56,13 +73,4 @@ public class PetMachine {
     }
     // limpar maquina
 
-
-    // getters
-
-    public int getWater() {
-        return this.water;
-    }
-    public int getShampoo() {
-        return shampoo;
-    }
 }
